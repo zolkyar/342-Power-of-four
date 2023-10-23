a36a9e87aff8ea12e1c3cab2e9b1728468b9699c
zolkyar <zolkyar@gmail.com>
2023-10-23 03:50:01 +0100

    Optimate first solution

--- main.kt
+++ main.kt
@@ -1,17 +1,14 @@
 class Solution {
-    fun isPowerOfFour(n: Int): Boolean {
-        if (n == 0) {return false}
-        else if (n == 1) {return true}
-        
+    fun isPowerOfFour(n: Int): Boolean {        
         var n = n
         var tmp = 1
         
         for (i in 1..31){
-            tmp = tmp * 4
             if(tmp > n) {return false}
             else if (n == tmp) {return true}
+            tmp = tmp * 4
         }
-        
+    
         return false
     }
 }
